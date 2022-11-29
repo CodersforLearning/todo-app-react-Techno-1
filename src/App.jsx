e818289e71fc73de1fc972abbcaca78dc12a9df9
root <root@Johns-PC>
2022-11-29 13:18:05 +0800

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,120 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
+      {
+        <TodoList />
+
+
+
+
+        // <div>
+        //   <div>
+        //       <h1>John's Todo List</h1>
+        //       <div id="numberOfTasks">
+        //           <h3>Number Of Tasks:&nbsp</h3>
+        //           <h3 id="taskNumber"></h3>
+        //       </div>
+        //   </div>
+        //   <div>
+        //       <form>
+        //           <input id="newTask" type="text" placeholder="Add task"></input>
+        //           <button id="submit">Submit</button>
+        //           <button id="tellJoke">Wanna hear a joke?</button>
+        //       </form>
+        //   </div>
+        //   <div>
+        //       <ul id="taskList">
+
+        //       </ul>
+        //   </div>
+        // </div>
+      }
+    </div>
+  )
+}
+
+function TodoList() {
+  return (
+    <div>
+      <Heading />
+      <TaskForm />
+      <TaskList />
+    </div>
+  )
+}
+
+function Heading() {
+  return (
+    <div>
+      <MainHeading />
+      <NumberOfTasks />
+    </div>
+  )
+}
+
+function MainHeading() {
+  return (
+    <h1>John's Todo List</h1>
+  )
+}
+
+function NumberOfTasks() {
+  return (
+    <div id="numberOfTasks">
+      <h3>Number Of Tasks:&nbsp</h3>
+      <h3 id="taskNumber"></h3>
+    </div>
+  )
+}
+
+function TaskForm() {
+  return (
+    <div>
+      <form>
+        <input id="newTask" type="text" placeholder="Add task"></input>
+        <button id="submit">Submit</button>
+        <button id="tellJoke" onClick={joke}>Wanna hear a joke?</button>
+      </form>
+    </div>
+  )
+}
+
+function TaskList() {
+  return (
+    <div>
+      <ul id="taskList">
+
+      </ul>
+    </div>
+  )
+}
+
+
+
+// Functions
+
+
+
+function joke(e) {
+  fetch('https://v2.jokeapi.dev/joke/Misc,Programming?format=json&safe-mode&type=single')
+  .then(response => response.json())
+  .then(data => {
+      console.log(data);
+      const theJoke = data.joke;
+      alert(theJoke);
+  });
+  
+  e.preventDefault();
+}
+
+ //App
+
+      /* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
@@ -26,9 +134,15 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+      </p> */
 
-export default App
+
+
+/* Component hierarchy
+  - TodoList
+    - Heading
+      - MainHeading
+      - NumberOfTasks
+    - TaskForm
+    - TaskList
+ */
